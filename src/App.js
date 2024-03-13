f142f82dcf3c079358532e6bcd3edb0cf6cb9334
Vinay Kumar <92167129+vina-y-kumar@users.noreply.github.com>
2024-03-13 13:19:46 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-// App.js
 import React from 'react';
 import { BrowserRouter as Router, Route, Link,  Routes } from 'react-router-dom';
 import Dashboard from './Dashboard';
@@ -9,23 +8,21 @@ import "./App.css";
 function App() {
   return (
     <Router>
-       <div className="container">
-        <header className="header">
-          <h1>Modern ERP System</h1>
-        </header>
-        <nav className="navbar">
-          <ul>
-            <li>
-              <Link to="/">Dashboard</Link>
-            </li>
-            <li>
-              <Link to="/products">Products</Link>
-            </li>
-            <li>
-              <Link to="/orders">Orders</Link>
-            </li>
-          </ul>
-        </nav>
+      <div>
+      <nav className="bg-gray-800 p-4 flex items-center justify-between">
+    <h1 className="text-white text-xl font-bold text-center">MODERN ERP SYSTEM</h1>
+    <ul className="flex space-x-4">
+      <li>
+        <Link to="/" className="text-white hover:text-gray-300">Dashboard</Link>
+      </li>
+      <li>
+        <Link to="/orders" className="text-white hover:text-gray-300">Orders</Link>
+      </li>
+      <li>
+        <Link to="/products" className="text-white hover:text-gray-300">Products</Link>
+      </li>
+    </ul>
+  </nav>
 
         <Routes>
           <Route path="/" element={<Dashboard/>} />
